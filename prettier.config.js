2d42d2f0787c84878068ad8d387f677143617b63
Jessie <zhangqian006@gmail.com>
2024-07-16 10:20:54 +1000

    first commit

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+// export default {
+//   plugins: [await import('prettier-plugin-tailwindcss')],
+//   singleQuote: true,
+// };
+
+// .prettierrc
+module.exports = {
+  plugins: ["prettier-plugin-tailwindcss"],
+};
+// module.exports = {
+//   plugins: [require("prettier-plugin-tailwindcss")],
+//   singleQuote: true,
+// };
